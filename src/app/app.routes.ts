d9bdf501bd45f39e72e558ae0390e185e8d4f02c
DuyChung <79136538+DuyChung@users.noreply.github.com>
2025-11-02 19:56:50 +0700

    fix task1 (#1)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: Home },
 ];
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  articles = [
+    { author: 'pradaman', title: 'Celular xiaomi', desc: 'bài tập hôm nay là về ', date: 'October 31, 2025' },
+    { author: 'testuser1761745955343', title: 'Original Title', desc: 'Original Description', date: 'October 29, 2025' }
+  ];
+
+  tags = ['frontend', 'backend', 'science', 'angular', 'typescript', 'test', 'example'];
 
 }
