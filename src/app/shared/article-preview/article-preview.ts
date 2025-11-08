114a0a132bb0574d3df68dc9f63d4af8a8c8e967
DuyChung <chungnguyen31@gmail.com>
2025-11-08 13:28:00 +0700

    task4 fix giao diện

---
+++ src/app/shared/article-preview/article-preview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-preview',
+  imports: [],
+  templateUrl: './article-preview.html',
+  styleUrl: './article-preview.scss',
+})
+export class ArticlePreview {
+
+}
